d8d280c4372a9a3afa46c5e2c614932211aadf53
Kevin Mitchell <loudwinston@gmail.com>
2018-03-18 23:08:49 -0400

    multiplication half of part 4 completed

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,42 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// initialize sum to zero
+	@sum
+	M = 0
+
+// set i to R0
+	@R0
+	D = M	
+	@i
+	M = D
+
+(LOOP)
+	// exit loop if i = 0
+		@i 
+		D = M
+		@END
+		D;JEQ
+
+	// subtract 1 from i
+		@i
+		M = M-1
+
+	// add R1 to sum
+		@R1
+		D = M
+		@sum
+		M = M + D // add R1 to @SUM
+	
+	// repeat loop
+		@LOOP
+		0;JMP
+(END)
+@END
+
+// set R2 to sum
+	@sum
+	D = M
+	@R2
+	M = D
+
